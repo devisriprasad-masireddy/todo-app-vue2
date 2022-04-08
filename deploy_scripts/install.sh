38fd036326617af79001ca085180c295af2d39b0
Devisriprasad Masireddy <devisriprasad@recruitcrm.io>
2022-04-08 15:10:59 +0530

    deploy-1

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -7,4 +7,5 @@ then
    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
    . ~/.nvm/nvm.sh
    nvm install 4.3.2
+   NODE_ENV=production npm run build
 fi
--- deploy_scripts/start.sh
+++ deploy_scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/todo-app
-service httpd start
+NODE_ENV=production npm run build
