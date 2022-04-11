fc45d8c77f2d453d73e7e653a3a9b04b7dc78b82
Devisriprasad Masireddy <100702420+devisriprasad-masireddy@users.noreply.github.com>
2022-04-11 14:08:17 +0530

    node install

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -4,8 +4,8 @@ cd /home/ec2-user
 node --version
 if [ $? -ne 0 ]
 then
-   curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
    . ~/.nvm/nvm.sh
-   nvm install 4.3.2
-   NODE_ENV=production npm run build
+   nvm install node
+   npm run build
 fi
