069534979c7bc8d19800202de348003ee1b0c244
Devisriprasad Masireddy <100702420+devisriprasad-masireddy@users.noreply.github.com>
2022-04-12 07:06:43 +0530

    to ubuntu

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
-cd /home/ec2-user
+source /home/ubuntu/.bash_profile
+cd /home/ubuntu
 node --version
 if [ $? -ne 0 ]
 then
    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
    . ~/.nvm/nvm.sh
    nvm install node
-   npm run build
 fi
