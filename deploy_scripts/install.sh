d21447cb0c15a0034cf865ca5b02b3347d509109
Devisriprasad Masireddy <100702420+devisriprasad-masireddy@users.noreply.github.com>
2022-04-19 11:59:23 +0530

    Update install.sh

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -4,7 +4,16 @@ cd /home/ubuntu
 node --version
 if [ $? -ne 0 ]
 then
-   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-   . ~/.nvm/nvm.sh
-   nvm install node
+    sudo apt update
+    sudo apt install ruby
+    sudo apt install wget
+    cd /home/ubuntu
+    sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+    sudo chmod +x ./install
+    sudo ./install auto
+    sudo apt install httpd
+    sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt install nodejs
+    cd /home/www/html
+    sudo npm i @vue/cli-service
 fi
